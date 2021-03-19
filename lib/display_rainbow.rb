dc8c1009bc85ffcd20910f0a55ffc25d0f2b506b
interactive-tuple-7017 <sarahharley@outlook.com>
2021-03-19 18:17:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+color = [R: Red, O: Orange, Y: Yellow, G: Green, B: Blue, I: Indigo, V: Violet]
+
+def display_rainbow(color)
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
+end
